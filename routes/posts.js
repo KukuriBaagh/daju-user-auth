6c9c2f0fb62df99dfd91f0d88ca166840a31adce
KukuriBaagh <howtoccdemo@gmail.com>
2023-11-10 21:09:15 +0530

    checkAuth middleware added

--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,13 @@
 const router = require('express').Router();
 const { publicPosts, privatePosts } = require('../db');
+const checkAuth = require('../middleware/checkAuth');
 
 router.get('/public', (req, res) => {
   res.json(publicPosts);
 });
 
-router.get(
-  '/private',
-  (req, res, next) => {
-    let userValid = false;
-    if (userValid) {
-      next();
-    } else {
-      return res.status(400).json({
-        errors: [
-          {
-            msg: 'unauthorized request, please login',
-          },
-        ],
-      });
-    }
-  },
-  (req, res) => {
-    res.json(privatePosts);
-  }
-);
+router.get('/private', checkAuth, (req, res) => {
+  res.json(privatePosts);
+});
 
 module.exports = router;
